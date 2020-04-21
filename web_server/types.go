d1e3b4033f7621eda27dde246dd208deab7f28b0
John <vojohn95@gmail.com>
2020-04-21 03:01:09 -0500

    Se crea apis

--- web_server/types.go
+++ web_server/types.go
@@ -1,6 +1,26 @@
 package main
 
-import "net/http"
+import "encoding/json"
 
-type Middleware func(handler http.HandlerFunc) http.HandlerFunc
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
 
+func (u *User) IsValid() bool {
+	if u.Email == "" {
+		return false
+	}
+	if u.Name == "" {
+		return false
+	}
+	if u.Phone == "" {
+		return false
+	}
+	return true
+}
+
+func (u *User) ToJson() ([]byte, error) {
+	return json.Marshal(u)
+}
